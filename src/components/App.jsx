2d59b4a9d1c21187a3c4beb375b1bac331d47dcf
ssvitlana31 <130747746+ssvitlana31@users.noreply.github.com>
2023-10-31 15:55:48 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,20 +7,19 @@ import { Filter } from './PhoneBook/Filter';
 import { Container, Title } from './PhoneBook/Form.styled';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
-  const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    const storageContacts = JSON.parse(window.localStorage.getItem('contacts'));
-    if (storageContacts) {
-      setContacts(storageContacts);
+  const [contacts, setContacts] = useState(() => {
+    const data = JSON.parse(window.localStorage.getItem('contacts'));
+    if (data?.length) {
+      return data;
     }
-  }, []);
+    return [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ];
+  });
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     window.localStorage.setItem('contacts', JSON.stringify(contacts));
